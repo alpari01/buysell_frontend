db96e862bfae135ee664727f00abf524dd8e5c05
illast <illast@ttu.ee>
2022-11-29 21:30:17 +0200

    All products displayed at products page

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,38 +1,78 @@
-<script setup>
+<script>
+import axios from "axios";
 
-// function addProduct() {
-//   if (isUserLoggedIn()) {
-//     const productTable = document.getElementById("productTable")
-//
-//     const newProduct = document.createElement("div")
-//     newProduct.setAttribute("class", "col")
-//
-//     const newProductLink = document.createElement("a")
-//     newProductLink.setAttribute("href", "www.google.com")
-//     newProductLink.innerHTML = "PRODUCT PAGE"
-//
-//     newProduct.appendChild(newProductLink)
-//
-//     productTable.appendChild(newProduct)
-//
-//     let userData = VueJwtDecode.decode(JSON.parse(localStorage.getItem("token")));
-//     console.log(userData["id"])
-//   }
-//
-//   else alert("User not logged in.");
-// }
+export default {
+  data() {
+    return {
+      products: [],
+      page: 0
+    }
+  },
+
+  methods: {
+    async goPageForward() {
+      if ((await axios.get("/api/public/products2?page=" + (this.page + 1) + "0&orderBy=id")).data.productList.length > 0) {
+        this.page++
+        this.products = (await axios.get("/api/public/products2?page=" + this.page + "0&orderBy=id")).data.productList
+        console.log(this.products)
+      }
+    },
+
+    async goPageBack() {
+      if (this.page > 0) {
+        this.page--
+        this.products = (await axios.get("/api/public/products2?page=" + this.page + "0&orderBy=id")).data.productList
+        console.log(this.products)
+      }
+    }
+  },
+
+  async created() {
+    this.products = (await axios.get("/api/public/products2?page=0&orderBy=id")).data.productList;
+  }
+}
 </script>
 
 <template>
-  <div class="col-sm-3 mx-auto">
-    <router-link to="/product/add"><input type="button" class="btn btn-primary btn-bg" value="Add new product"></router-link>
+  <div class="col-sm-6 mx-auto">
+    <input type="button" v-on:click="goPageBack" class="btn btn-primary btn-sm" style="margin-right: 5px" value="←">
+    <input type="button" v-on:click="goPageForward" class="btn btn-primary btn-sm" style="margin-left: 5px" value="→">
   </div>
-  <div class="container text-center">
-    <div class="row row-cols-4" id="productTable">
-    </div>
+
+  <div class="col-sm-3 mx-auto">
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+      </tr>
+      <tr v-for="product of products" :key="product.id">
+        <td>{{ product.id }}</td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.description }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <style scoped>
+figcaption {
+  font-size: 35px
+}
 
+table {
+  border-collapse: separate;
+  border-spacing: 0 20px;
+  font-size: 20px;
+  word-break: break-word;
+}
+th {
+  border-bottom: 1px solid #3a3a3a;
+}
+td {
+  width: 150px;
+  text-align: center;
+  padding: 5px;
+  border-bottom: 1px solid #ababab;
+}
 </style>
